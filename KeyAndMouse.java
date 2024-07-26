6399161278263e2c82da527faf26dc2e36905674
SAVVY1710 <163214365+SAVVY1710@users.noreply.github.com>
2024-07-26 03:57:45 -0700

    added timers classes and paintcomponent

--- KeyAndMouse.java
+++ KeyAndMouse.java
@@ -63,4 +63,71 @@ class DrawingPanel extends JPanel implements KeyListener
         car2timer = new Timer(10, car2mover);
         car2timer.start();
     }
+    class BallMover implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // ball XY location
+            ballY += ballspeed;
+            if (ballY > 470) {
+                ballY = 10;
+                ballX = (int)(Math.random() * 470) + 1;
+            }
+            repaint();
+            grabFocus();
+        }
+    } // end BallMover
+
+    class CarMover implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // car XY location
+            carX+= carspeed;
+            if (carX > 490 ) carX = 0;
+            repaint();
+            grabFocus();
+        }
+    } // end CarMover
+
+    class Car2Mover implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // car XY location
+            car2X += -1*carspeed;
+            if(car2X < 0)car2X = 490;
+            repaint();
+            grabFocus();
+        }
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        setBackground(Color.gray);
+
+        g.setColor(Color.red);
+        g.setFont(new Font("Serif", Font.BOLD, 30));
+        g.drawString("Press number for ball", 20, 50);
+        g.drawString("Press vowel for orange car", 20, 100);
+        g.drawString("Press key from bottom row for blue car", 20, 150);
+
+        //g.drawString ("Press mouse on the panel.  To increase the font size, press the UP arrow.  ", 50, 300);
+        //g.drawString("To make the screen darker, click the box labelled darker", 50, 400);
+        //g.drawString("To make the screen brighter, click the box labelled brighter" , 50, 500);
+        //g.drawString("To make the screen darker, click the box labelled darker", 50, 600);
+
+        // paint ball
+        if(ballcolor == 0 || ballcolor%2 == 0)g.setColor(Color.pink);
+        else if(ballcolor%2 == 1)g.setColor(Color.green);
+
+        g.fillOval(ballX, ballY, 30, 30);
+
+        // paint car
+        g.setColor(Color.orange);
+        g.fillOval(carX+2, carY, 20, 20); // back wheel
+        g.fillOval(carX+40, carY, 20, 20); // front wheel
+        g.fillRect(carX, carY-20, 60, 20); // car body
+        g.fillRect(carX+10, carY-35, 40, 15); // car top
+
+        // paint car
+        g.setColor(Color.blue);
+        g.fillOval(car2X+2, car2Y, 20, 20); // back wheel
+        g.fillOval(car2X+40, car2Y, 20, 20); // front wheel
+        g.fillRect(car2X, car2Y-20, 60, 20); // car body
+        g.fillRect(car2X+10, car2Y-35, 40, 15); // car top
+    } // end paintComponent
 }
